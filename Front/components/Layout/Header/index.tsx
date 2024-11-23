935a8ba2af09b7195ea63f01aee2ce66692773fa
vandilsonbrito <vandilson25@outlook.com>
2024-11-23 00:35:12 -0300

    feat: create 'Header' component

--- Front/components/Layout/Header/index.tsx
+++ Front/components/Layout/Header/index.tsx
@@ -2,18 +2,33 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react';
 import Logo from '../../../public/images/eco-food-logo.png'
+import Button from '@/components/Button';
 
 export default function index() {
   return (
-    <header className='w-full h-20 flex items-center justify-between'>
+    <header className='w-full h-20 flex items-center justify-between bg-principalColor text-white px-6'>
         <Link href='/'>
             <Image
-                src={}
+                src={Logo}
                 alt="EcoFood Logo"
-                width={80}
-                height={80}
+                width={100}
+                height={100}
             />
         </Link>
+
+        <div className="flex items-center gap-5">
+          <Link href='/'>
+            <Button className='bg-transparent text-[1.05rem] hover:bg-buttonBgColor'>
+                Login
+            </Button>
+          </Link>
+          <Link href='/'>
+            <Button className='bg-transparent text-[1.05rem] hover:bg-buttonBgColor'>
+                Cadastre-se
+            </Button>
+          </Link>
+        </div>
+        
     </header>
   )
 }
