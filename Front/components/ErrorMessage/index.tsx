0eac3243ac04238ba55400d0d6255b765aa0ba4c
Gabriel Melo <bielrj170@gmail.com>
2024-11-23 14:14:03 -0300

    refactor: refactor text size

--- Front/components/ErrorMessage/index.tsx
+++ Front/components/ErrorMessage/index.tsx
@@ -1,5 +1,5 @@
 const ErroMessage = ({ children }: { children: React.ReactNode }) => {
-	return <span className="text-red-500 text-sm">{children}</span>;
+	return <span className="text-red-500 text-[10px]">{children}</span>;
 };
 
 export default ErroMessage;
