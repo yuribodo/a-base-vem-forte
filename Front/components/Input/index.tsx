8c5b745077331b2900b40e2f981c266a968ad34e
Gabriel Melo <bielrj170@gmail.com>
2024-11-23 01:11:01 -0300

    refactor: put ref into input

--- Front/components/Input/index.tsx
+++ Front/components/Input/index.tsx
@@ -1,31 +1,34 @@
-'use client';
+"use client";
 
-import React from "react";
+import React, { forwardRef } from "react";
 
 interface InputProps {
-  type?: "text" | "email" | "password" | "number" | "tel" | "date";
-  placeholder?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  className?: string; 
+	type?: "text" | "email" | "password" | "number" | "tel" | "date";
+	placeholder?: string;
+	value?: string;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	className?: string;
+	ref?: React.Ref<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({
-  type = "text",
-  placeholder = "",
-  value,
-  onChange,
-  className = "",
-}) => {
-  return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className={`w-full px-4 py-2 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-[#3a8b40] focus:border-[#3a8b40] transition-all ${className}`}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+	(
+		{ type = "text", placeholder = "", value, onChange, className = "" },
+		ref
+	) => {
+		return (
+			<input
+				ref={ref}
+				type={type}
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+				className={`w-full px-4 py-2 text-sm border rounded-md focus:outline-none focus:ring-1 focus:ring-[#3a8b40] focus:border-[#3a8b40] transition-all ${className}`}
+			/>
+		);
+	}
+);
+
+Input.displayName = "Input";
 
 export default Input;
