971a95119bc9313e4c21ef026e698e6bc654939e
Gabriel Melo <bielrj170@gmail.com>
2024-11-23 03:23:13 -0300

    refactor: change Login Page folder

--- Front/UIPages/Login/index.tsx
+++ Front/UIPages/Login/index.tsx
@@ -3,7 +3,7 @@ import FormLogin from "@/components/FormLogin";
 
 const LoginPage = () => {
 	return (
-		<div className="bg-transparent min-w-[400px] min-h-[390px] mx-auto flex flex-col items-center px-6">
+		<div className="flex flex-col items-center">
 			<EcoLogo height={100} width={450} />
 			<FormLogin />
 		</div>
