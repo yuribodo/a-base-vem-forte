55bc8c7a5068825a272bbac683d94e036135c67c
vandilsonbrito <vandilson25@outlook.com>
2024-11-23 00:37:08 -0300

    feat: add 'Header' component

--- Front/app/layout.tsx
+++ Front/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import Header from "../components/Layout/Header";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Header/>
+				{children}
+			</body>
 		</html>
 	);
 }
