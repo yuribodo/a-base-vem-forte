73dc33ddb944430895ad6863bf6a71113b4b3a52
Gabriel Melo <bielrj170@gmail.com>
2024-11-23 14:14:18 -0300

    refactor: refactor margin size

--- Front/UIPages/Register/index.tsx
+++ Front/UIPages/Register/index.tsx
@@ -3,7 +3,7 @@ import FormRegister from "@/components/FormRegister";
 
 const RegisterPage = () => {
 	return (
-		<div className="flex flex-col items-center">
+		<div className="flex flex-col items-center my-4">
 			<EcoLogo height={100} width={450} />
 			<FormRegister />
 		</div>
