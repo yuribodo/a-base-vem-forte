93d50a5b31052d33b9e444b1a540ed7271251fd2
Duanne Moraes <duanne.souza@gmail.com>
2024-11-23 11:20:13 -0300

    feat: Create log model

--- Backend/src/log/models.py
+++ Backend/src/log/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import User
+from products.models import Products
 
-# Create your models here.
+
+class Log(models.Model):
+    ACTION_CHOICE = [
+        ("UPDATE_QUANTITY", "Update the quantity"),
+        ("UPDATE_PRICE", "Update the price"),
+    ]
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="logs"
+    )
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="logs")
+    action = models.CharField(max_length=50, choices=ACTION_CHOICE)
+    timestamp = models.DateTimeField(auto_now_add=True)
