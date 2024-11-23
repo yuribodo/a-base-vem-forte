2171d35505855ee1930a6de4a9a33bafa8b85c9b
Duanne Moraes <duanne.souza@gmail.com>
2024-11-23 17:15:05 -0300

    feat: Create log view

--- Backend/src/log/views.py
+++ Backend/src/log/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Log
+from .serializers import LogSerializer
 
-# Create your views here.
+
+class LogListCreateView(generics.ListCreateAPIView):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class LogRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
