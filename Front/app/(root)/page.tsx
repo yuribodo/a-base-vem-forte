87e04dee33e98676d6b0617a4720208090d91829
yuribodo <yemotalara@gmail.com>
2024-11-24 12:27:00 -0400

    landing

--- Front/app/(root)/page.tsx
+++ Front/app/(root)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<main className="w-full h-full min-h-screen">
 			<Banner />
-			<FeaturesSection />
+			{/* {<FeaturesSection />} */}
 		</main>
 	);
 }
