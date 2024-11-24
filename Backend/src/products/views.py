68578afa965facd7ce32a7f57c2209995539a266
Duanne Moraes <duanne.souza@gmail.com>
2024-11-24 11:53:32 -0300

    feat: Put decorator extend schema for documentation

--- Backend/src/products/views.py
+++ Backend/src/products/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from .models import Products
 from .serializers import ProductSerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Products"])
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
 
 
+@extend_schema(tags=["Products"])
 class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
