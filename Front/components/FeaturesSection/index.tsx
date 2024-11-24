1cebd2f6c3007095efb9bb5e94b140cbc8a52898
vandilsonbrito <vandilson25@outlook.com>
2024-11-24 03:32:56 -0300

    feat: create FeaturesSection component

---
+++ Front/components/FeaturesSection/index.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function index() {
+  return (
+    <section className='w-full h-screen bg-principalColor p-7 pt-12'>
+        <h2 className='text-3xl text-center text-white uppercase font-semibold'>Funcionalidades</h2>
+
+        <div className="w-full h-full flex gap-10 px-10 justify-center items-center">
+            <div className="w-full max-w-[320px]">
+                <Image
+                    src={""}
+                    alt='Dasboard da EcoFood'
+                    width={200}
+                    height={200}
+                />
+                <p className='text-white text-lg font-medium'>Dasboard interativo com informações sobre os produtos</p>
+            </div>
+            <div className="w-full max-w-[320px]">
+                <Image
+                    src={""}
+                    alt='Dasboard da EcoFood'
+                    width={200}
+                    height={200}
+                />
+                <p className='text-white text-lg font-medium'>Relatórios detalhado sobre os produtos</p>
+            </div>
+            <div className="w-full max-w-[320px]">
+                <Image
+                    src={""}
+                    alt='Dasboard da EcoFood'
+                    width={200}
+                    height={200}
+                />
+                <p className='text-white text-lg font-medium'>Dados atualizados em tempo real</p>
+            </div>
+        </div>
+    </section>
+  )
+}
