a38f7b6ddd5ef9c16137ccd553cc242edfd9a3aa
vandilsonbrito <vandilson25@outlook.com>
2024-11-24 03:33:34 -0300

    feat: add Banner and FeaturesSection components

--- Front/app/(root)/page.tsx
+++ Front/app/(root)/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import useAuthContext from "@/hooks/useAuthContext";
 import React from "react";
+import Banner from "../../components/Banner";
+import FeaturesSection from "../../components/FeaturesSection";
 
 export default function Home() {
 	const { user } = useAuthContext();
 
 	return (
-		<>
-			<p>oi</p>
-		</>
+		<main className="w-full h-full min-h-screen">
+			<Banner/>
+			<FeaturesSection/>
+		</main>
 	);
 }
