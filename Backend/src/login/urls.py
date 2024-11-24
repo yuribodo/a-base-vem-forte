abd5fa50f880248306da9f86a0b5b0919e59c78c
Duanne Moraes <duanne.souza@gmail.com>
2024-11-24 11:50:44 -0300

    feat: Create token refresh route

--- Backend/src/login/urls.py
+++ Backend/src/login/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import LoginView
 from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "token/refresh/",
+        extend_schema_view(post=extend_schema(tags=["Login"]))(
+            TokenRefreshView.as_view()
+        ),
+        name="token_refresh",
+    ),
 ]
