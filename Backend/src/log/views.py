138b045a970af44e7e765a2b89fcac191d80c9c3
Duanne Moraes <duanne.souza@gmail.com>
2024-11-24 11:49:30 -0300

    feat: Put decorator extend schema for documentation

--- Backend/src/log/views.py
+++ Backend/src/log/views.py
@@ -2,8 +2,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Log
 from .serializers import LogSerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Log"])
 class LogListCreateView(generics.ListCreateAPIView):
     queryset = Log.objects.all()
     serializer_class = LogSerializer
@@ -13,6 +15,7 @@ class LogListCreateView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+@extend_schema(tags=["Log"])
 class LogRetrieveDestroyView(generics.RetrieveDestroyAPIView):
     queryset = Log.objects.all()
     serializer_class = LogSerializer
