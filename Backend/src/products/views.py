cdc2879fbb0fc6bc748180c27e04a1571f625bfa
Duanne Moraes <duanne.souza@gmail.com>
2024-12-16 15:23:46 -0300

    refactor: Change imports in products view

--- Backend/src/products/views.py
+++ Backend/src/products/views.py
@@ -1,16 +1,16 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Products
 from .serializers import ProductSerializer
 from drf_spectacular.utils import extend_schema
 
 
 @extend_schema(tags=["Products"])
-class ProductListCreateView(generics.ListCreateAPIView):
+class ProductListCreateView(ListCreateAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
 
 
 @extend_schema(tags=["Products"])
-class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
