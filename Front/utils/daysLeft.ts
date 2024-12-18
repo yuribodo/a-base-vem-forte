dcf4ca2bcfc400533d5389bc4d3edd998ba1c4d7
Gabriel Melo <bielrj170@gmail.com>
2024-12-17 23:40:27 -0300

    refactor: adjust data logic to pick from api

--- Front/utils/daysLeft.ts
+++ Front/utils/daysLeft.ts
@@ -1,8 +1,8 @@
-import { MockedProducts } from "@/components/TableProducts";
+import { Product } from "@/components/TableProducts";
 
-export const daysLeft = (product: MockedProducts) => {
+export const daysLeft = (product: Product) => {
 	const today = new Date();
-	const expirationDate = new Date(product.validade);
+	const expirationDate = new Date(product.expiration_date);
 	const timeDiff = expirationDate.getTime() - today.getTime();
 	const days = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 	return days;
