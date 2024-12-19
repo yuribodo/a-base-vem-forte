25296964ef538a0c75c61341bc3ad72c0c915cd2
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2024-12-19 00:41:49 -0300

    feat: create product API service functions

---
+++ Front/utils/productApiService.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const BASE_URL = "http://127.0.0.1:8000";
+
+export const handleRecycle = async (selectedProductId: number | null) => {
+    if(!selectedProductId) return;
+    try {
+        const response = await axios.patch(`${BASE_URL}/api/products/${selectedProductId}/status/`, {
+            destination: 'DONATION'
+        });
+        console.log("Product recycled:", response.data);
+        return 'success';
+    }
+    catch (error) {
+        console.error("Error updating product:", error);
+    }
+}
+export const handleDiscard = async (selectedProductId: number | null) => {
+    if(!selectedProductId) return;
+    try {
+        const response = await axios.patch(`${BASE_URL}/api/products/${selectedProductId}/status/`, {
+            destination: 'TRASH'
+        });
+        console.log("Product discarded:", response.data);
+        return 'success';
+    }
+    catch (error) {
+        console.error("Error updating product:", error);
+    }
+}
+
+export const handleEdit = async (selectedProductId: number | null) => {
+    if(!selectedProductId) return;
+    try {
+        const response = await axios.patch(`${BASE_URL}/api/products/${selectedProductId}/`, {
+            
+        });
+        console.log("Product edited:", response.data);
+        return 'success';
+    } 
+    catch (error) {
+        console.error("Error updating product:", error);
+    }
+}
+export const handleDelete = async (selectedProductId: number | null) => {
+    if(!selectedProductId) return;
+    try {
+        const response = await axios.delete(`${BASE_URL}/api/products/${selectedProductId}/`);
+        console.log("Product deleted:", response.data);
+        return 'success';
+    }
+    catch (error) {
+        console.error("Error deleting product:", error);
+    }
+}
