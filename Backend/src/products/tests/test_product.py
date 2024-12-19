f0471eeb4612b1b6dbf530256fb117dec2cb13ef
Duanne Moraes <duanne.souza@gmail.com>
2024-12-19 15:57:57 -0300

    feat: Create test to verify creation a new product

---
+++ Backend/src/products/tests/test_product.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from products.models import Products
+
+
+class ProductTestCase(TestCase):
+    fixtures = ["prototype_db.json"]
+
+    def setUp(self):
+        self.product = Products.objects.get(pk=1)
+        self.list_create_url = reverse("product-list-create")
+        self.detail_url = reverse("product-detail", args=[self.product.id])
+        self.update_url = reverse("product-recycle-or-discard", args=[self.product.id])
+
+    def test_create_product(self):
+        """Test creating a new product"""
+
+        new_product_data = {
+            "name": "New Product",
+            "description": "Another product description",
+            "category": "MEATS",
+            "price": "15.50",
+            "expiration_date": "2024-12-31",
+            "quantity": 5,
+            "code_product": "NP67890",
+            "destination": "DONATION",
+            "is_perishable": True,
+            "date_of_manufacture": "2024-01-01",
+            "recycle": False,
+            "discard": False,
+        }
+        response = self.client.post(
+            self.list_create_url, new_product_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Products.objects.count(), 3)
