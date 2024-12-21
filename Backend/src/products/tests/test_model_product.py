62a69183a1b977a4d7042c251a573593af0c0901
Duanne Moraes <duanne.souza@gmail.com>
2024-12-21 13:02:59 -0300

    feat: Create test to verify if that code_product field enforces uniqueness

--- Backend/src/products/tests/test_model_product.py
+++ Backend/src/products/tests/test_model_product.py
@@ -13,12 +13,12 @@ class ProductsModelTestCase(TestCase):
             "description": "Product descriptions",
             "category": "MEATS",
             "price": "100.00",
-            "expiration_date": "2024-12-27",
+            "expiration_date": parse_date("2024-12-27"),
             "quantity": 10,
             "code_product": "TEST123",
             "destination": "TRASH",
             "is_perishable": True,
-            "date_of_manufacture": "2024-09-24",
+            "date_of_manufacture": parse_date("2024-09-24"),
             "recycle": False,
             "discard": False,
             "total_recycled": 0,
@@ -40,8 +40,14 @@ class ProductsModelTestCase(TestCase):
             product.full_clean()
 
     def test_destination_choices_validation(self):
-        """test tha invalid destination values raise ValidadetionError"""
+        """test tha invalid destination values raise ValidationError"""
         self.product_data["destination"] = "INVALID"
         with self.assertRaises(ValidationError):
             product = Products(**self.product_data)
             product.full_clean()
+
+    def test_destination_choices_validation(self):
+        """Test tha code_product field enforces uniqueness"""
+        Products.objects.create(**self.product_data)
+        with self.assertRaises(Exception):
+            Products.objects.create(**self.product_data)
